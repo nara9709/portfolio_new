b50bd6570ee96eff834209eeb619ce7645877494
nara9709 <worldkr19@gmail.com>
2022-07-28 22:57:17 -0400

    About me section and Yellow banner

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
 'use strict';
-
-function toggleHidden(className) {
-  const hoverMenu = document.getElementsByClassName(`nav-menu ${className}`);
-  const hoverImg = document.getElementById(`${className}`);
-
-  hoverImg.classList.toggle('hidden');
-}
