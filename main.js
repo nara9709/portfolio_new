a02ec9d60d01e4561f457b75d8e76babc09b1f2e
nara9709 <worldkr19@gmail.com>
2022-09-05 21:31:02 -0400

    Language setting and responsive size

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
 'use strict';
 
+let lang = 'korean';
+
+// If user click language button, change language setting
+document.getElementById('lang-icon').addEventListener('click', () => {
+  if (lang === 'korean') {
+    lang = 'english';
+
+    changeLangContent();
+  } else {
+    lang = 'korean';
+
+    changeLangContent();
+  }
+});
+
+function changeLangContent() {
+  console.log(lang);
+  let aboutme = document.querySelector('.aboutme-introduction');
+
+  if (lang === 'english') {
+    const intrductionEng = `I am creating a web and application for users, not is easy tocreate.
+    My goal is one, building the web that people love. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae dolorem voluptatum cumque voluptas aspernatur officia assumenda blanditiis quo corporis animi deserunt officiis necessitatibus delectus aut, est nulla quibusdam molestiae beatae.`;
+
+    aboutme.innerText = intrductionEng;
+  } else {
+    const introductionKr = `안녕하세요, 신입 front-end 개발자 이나라입니다.
+    <br> 기능을 그저 구현 하는것에 초점을 맞추는것이 아닌 필요한 기능을 <strong> '어떻게' </strong> 구현 할것이며 <strong>'왜'</strong> 이렇게 구현 해야만 하는지에 초점을 맞춰 개발하려고 노력하고 있습니다. <br> <br> 또한 무수히 쏟아지는 새로운 언어와 라이브러리, 기능들을 열린 마음으로 배워보려고 하고 있지만 그와 동시에 지금 내가 배우고 있는 것이 내게 필요한 이유를 확실히 하려고 하는 <br><strong> '이유있는 개발자'</strong>를 모토로 개발을 해왔습니다.
+ `;
+
+    aboutme.innerHTML = introductionKr;
+  }
+}
+
 // Make navbar transparent when it is on the top
 // const navbar = document.querySelector('#sec-nav');
 // const navbarHeight = navbar.getBoundingClientRect().height;
