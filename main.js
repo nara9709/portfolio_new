39f88ba21196f6096492e930fe0b73c7f5e9bf01
nara9709 <worldkr19@gmail.com>
2022-09-07 03:17:33 -0400

    Responsive Nav menu

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 
 let lang = 'korean';
 
+const burgerMenu = document.querySelector('#nav-burgermenu');
+const closeMenu = document.querySelector('#nav-closemenu');
+
 // If user click language button, change language setting
 document.getElementById('lang-icon').addEventListener('click', () => {
   if (lang === 'korean') {
@@ -25,13 +28,31 @@ function changeLangContent() {
 
     aboutme.innerText = intrductionEng;
   } else {
-    const introductionKr = `안녕하세요, 신입 front-end 개발자 이나라입니다.
-    <br> 기능을 그저 구현 하는것에 초점을 맞추는것이 아닌 필요한 기능을 <strong> '어떻게' </strong> 구현 할것이며 <strong>'왜'</strong> 이렇게 구현 해야만 하는지에 초점을 맞춰 개발하려고 노력하고 있습니다. <br> <br> 또한 무수히 쏟아지는 새로운 언어와 라이브러리, 기능들을 열린 마음으로 배워보려고 하고 있지만 그와 동시에 지금 내가 배우고 있는 것이 내게 필요한 이유를 확실히 하려고 하는 <br><strong> '이유있는 개발자'</strong>를 모토로 개발을 해왔습니다.
+    const introductionKr = `기능을 그저 구현 하는것에 초점을 맞추는것이 아닌 필요한 기능을 <strong> '어떻게' </strong> 구현 할것이며 <strong>'왜'</strong> 이렇게 구현 해야만 하는지에 초점을 맞춰 개발하려고 노력하고 있습니다. <br> <br> 무수히 쏟아지는 새로운 언어와 라이브러리, 기능들을 열린 마음으로 배워보려고 하고 있지만 그와 동시에 지금 내가 배우고 있는 것이 내게 필요한 이유를 확실히 하려고 하는 <br><strong> '이유있는 개발자'</strong>를 모토로 삼으며 개발하고 있습니다.
  `;
 
     aboutme.innerHTML = introductionKr;
   }
 }
+
+// Handle click event on Burger menu icon
+burgerMenu.addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('hidden');
+
+  burgerMenu.classList.toggle('hidden');
+
+  closeMenu.classList.toggle('hidden');
+});
+
+// Handle click event on close menu icon
+
+closeMenu.addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('hidden');
+
+  burgerMenu.classList.toggle('hidden');
+
+  closeMenu.classList.toggle('hidden');
+});
 
 // Make navbar transparent when it is on the top
 // const navbar = document.querySelector('#sec-nav');
